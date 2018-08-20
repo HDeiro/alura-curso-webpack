c66fcdd9851aa21f979a937f3398ccb8af75b6cf
ZSOUHUG <hugo.d.souza@ericssoninovacao.com.br>
2018-08-20 13:13:53 -0300

    Class 04 (Part 1) - CSS as Modules

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -13,6 +13,5 @@ const config = {
     headers,
     body 
 };
-
-fetch('/negociacoes', config)
+fetch('http://localhost:3000/negociacoes', config)
     .then(() => console.log('Dado enviado com sucesso'));
